8671fbc3fc12a269cc02f465ee1e58fe50b55904
ravjot07 <ravu2004@gmail.com>
2025-09-02 17:54:40 +0530

    Basic data types
    Signed-off-by: ravjot07 <ravu2004@gmail.com>

---
+++ src/api/events/routing.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from .schemas import EventSchema
+
+router = APIRouter()
+
+@router.get("/")
+def read_events():
+    return {
+        "items": [1,2,3]
+    }
+
+@router.get("/{event_id}")
+def get_event(event_id: int) -> EventSchema:
+    return {
+        "id": event_id
+    }
